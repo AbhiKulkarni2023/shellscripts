56b4ddcce115d8f254df84fadc19dd2c431e3c05
AbhiKulkarni2023 <abhijeetk396@example.com>
2023-08-31 23:29:48 +0530

    this is my first commit in branch monday

--- Script1.sh
+++ Script1.sh
@@ -4,7 +4,6 @@ echo "Hello All, Welcome to AWS/Devops Class"
 echo "Hello, How are you?"
 echo "Welcome to Young Minds"
 echo "Hello batch-19, We are learning Develops"
-echo "Hello All, Welcome to AWS/Devops Class"
-echo "Today is last day of class"
-echo "ready for mock"
+echo "today is mock"
+
 
